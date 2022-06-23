81f4c63c83ff8ce704336517a3309a2f18f9e7a5
mobai-001 <cbmb001@163.com>
2022-06-23 10:10:54 +0800

    Site updated: 2022-06-23 10:10:53

--- js/main.js
+++ js/main.js
@@ -777,24 +777,3 @@ document.addEventListener('DOMContentLoaded', function () {
   refreshFn()
   unRefreshFn()
 })
-
-fish();
-function fish() {
-    return (
-      $("#footer-wrap").css({
-        position: "absolute",
-        "text-align": "center",
-        top: 0,
-        right: 0,
-        left: 0,
-        bottom: 0,
-      }),
-      $("footer").append(
-        '<div class="container" id="jsi-flying-fish-container"></div>'
-      ),
-      $("body").append(
-        '<script src="https://raw.githubusercontent.com/mobai-001/MyHexoBlog.github.io/gh-pages/My.js"></script>'
-      ),
-      this
-    );
-  }
